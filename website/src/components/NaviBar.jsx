a54e4238afa519fae8deca51dfd4c8cc4000176d
angelshau <shauangel2000@gmail.com>
2025-02-25 18:02:59 -0500

    Update web struct

---
+++ website/src/components/NaviBar.jsx
@@ -0,0 +1,30 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+
+function NaviBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top shadow">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Digital Life</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/photo">Photo</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/action1">Action</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/action2">Another Action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/separated-link">Separated Link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NaviBar;
---
+++ website/src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+  return <h1 className="container mt-5 pt-5 text-center text-2xl">Welcome to Home Page</h1>;
+}
+export default Home;
